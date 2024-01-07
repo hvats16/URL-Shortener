9a4fc8708b33b4cf305ff342e8fb31de30e4443d
Harshit Vats <hvats16@gmail.com>
2024-01-07 13:21:56 +0530

    MongoDB connection and controller functions

---
+++ controllers/url.js
@@ -0,0 +1,23 @@
+const nanoid = require("shortid");
+const URL = require("../models/url");
+
+async function handleGenerateShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+  const shortID = nanoid(8);
+  await URL.create({
+    shortId: shortID,
+    redirect: body.url,
+    visitHistory: [],
+  });
+
+  return res.json({
+    id: shortID,
+  });
+}
+
+module.exports = {
+  handleGenerateShortURL,
+};
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const URLRoute = require("./routes/url");
+const connectToMongoose = require("./connect");
 
 const app = express();
 const PORT = 8001;
+
+connectToMongoose("mongodb://127.0.0.1:27017/short-url")
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+app.use("/url", URLRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
